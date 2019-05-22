03b9a1450c4aa8ef0e3b2602f03fb16f6c0c5664
Yash Raj <yashrj130@gmail.com>
2019-05-22 15:24:18 +0530

    Create index.js

---
+++ Flex Panel Gallery/index.js
@@ -0,0 +1,20 @@
+const panels = document.querySelectorAll(".panel");
+
+function togglePanel() {
+	this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+	//console.log(e.propertyName);
+	if(e.propertyName.includes("flex")) {
+		this.classList.toggle('open-active');	
+	}
+}
+
+panels.forEach((panel) => {
+	panel.addEventListener("click", togglePanel);
+});
+
+panels.forEach((panel) => {
+	panel.addEventListener("transitionend", toggleActive);
+});
