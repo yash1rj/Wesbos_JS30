69efafdb43f6d21e5632e9241f779ae7fba69370
Yash Raj <yashrj130@gmail.com>
2019-06-03 15:39:47 +0530

    Add files via upload

---
+++ Adding Video Duration with Reduce/index.js
@@ -0,0 +1,31 @@
+const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
+
+const seconds = timeNodes
+	.map(node => node.dataset.time)
+	.map(timeCode => {
+		const [mins, secs] = timeCode.split(':').map(parseFloat);
+		return (mins * 60) + secs;
+	})
+	.reduce((total, vidSeconds) => total + vidSeconds);
+	
+let secondsLeft = seconds;
+
+const hours = Math.floor(secondsLeft / 3600);
+
+secondsLeft = secondsLeft % 3600;
+
+const mins = Math.floor(secondsLeft / 60);
+
+secondsLeft = secondsLeft % 60;
+
+var myVar;
+
+function myFunction() {
+	myVar = setTimeout(alertFunc, 1000);
+}
+
+function alertFunc() {
+	alert(`Total video duration => HH,MM,SS -> ${hours}:${mins}:${secondsLeft}`);
+}
+
+myFunction();
