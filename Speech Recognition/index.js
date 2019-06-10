2669e0c49b5ca33440ec66a7c8be9219ccf4b327
yash1rj <yashrj130@gmail.com>
2019-06-10 23:59:54 +0530

    Update index.js

--- Speech Recognition/index.js
+++ Speech Recognition/index.js
@@ -14,7 +14,10 @@ const transcript = Array.from(e.results)
         .map(result => result.transcript)
         .join("")
 
-    p.textContent = transcript;
+
+    const poopScript = transcript.replace(/poop|poo|shit|dump/gi, '💩');
+    p.textContent = poopScript;
+
     if(e.results[0].isFinal) {
         p = document.createElement("p");
         words.appendChild(p);
